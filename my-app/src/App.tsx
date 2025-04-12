1b6dbaf6e1922faeae4410789ab82c35c1c296b0
FrederickIngle <fingle2016@fau.edu>
2025-04-12 01:14:00 -0400

    three content cards

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -8,7 +8,8 @@ function App() {
 
   return (
     <>
-  <div className="min-h-screen bg-base-200">
+  <div className="wrapper min-h-screen bg-base-200">
+    {/* Navbar */}
     <div className="navbar bg-base-100 shadow-sm">
       <div className="flex-1">
         <a className="btn btn-ghost text-xl">daisyUI</a>
@@ -38,6 +39,59 @@ function App() {
         </div>
       </div>
     </div>
+    {/*CARD WRAPPER */}
+    <div className="flex flex-wrap justify-center gap-6 p-4">
+      {/* CARD 1*/}
+      <div className="card bg-base-100 w-96 shadow-sm">
+      <figure>
+        <img
+          src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+          alt="Shoes" />
+      </figure>
+      <div className="card-body">
+        <h2 className="card-title">Card Title</h2>
+        <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+        <div className="card-actions justify-end">
+          <button className="btn btn-primary">Buy Now</button>
+        </div>
+      </div>
+    </div>
+    {/*CARD 2*/}
+    <div className="card bg-base-100 w-96 shadow-sm">
+      <figure>
+        <img
+          src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+          alt="Shoes" />
+      </figure>
+      <div className="card-body">
+          <h2 className="card-title">Card Title</h2>
+          <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+          <div className="card-actions justify-end">
+            <button className="btn btn-primary">Buy Now</button>
+          </div>
+      </div>
+    </div>
+    {/*CARD 3*/}
+    <div className="card bg-base-100 w-96 shadow-sm">
+      <figure>
+        <img
+          src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+          alt="Shoes" />
+      </figure>
+      <div className="card-body">
+          <h2 className="card-title">Card Title</h2>
+          <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+          <div className="card-actions justify-end">
+            <button className="btn btn-primary">Buy Now</button>
+          </div>
+      </div>
+    </div>
+  </div>
+    
+    
+
+   
+
   </div>
 
     
