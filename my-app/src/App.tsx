3d83e596f96ba4fa1c0a9484bf125635bc7e5e6f
FrederickIngle <fingle2016@fau.edu>
2025-04-15 22:59:35 -0400

    Stock Tracker component with api set up

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import SpendingChart from './components/SpendingChart';
 import './App.css'
+import StockTracker from './components/StockTracker';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -57,17 +58,8 @@ function App() {
 
     {/*CARD 2*/}
     <div className="card bg-base-100 w-96 shadow-sm">
-      <figure>
-        <img
-          src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-          alt="Shoes" />
-      </figure>
       <div className="card-body">
-          <h2 className="card-title">Card Title</h2>
-          <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
-          <div className="card-actions justify-end">
-            <button className="btn btn-primary">Buy Now</button>
-          </div>
+      <StockTracker />
       </div>
     </div>
     {/*CARD 3*/}
